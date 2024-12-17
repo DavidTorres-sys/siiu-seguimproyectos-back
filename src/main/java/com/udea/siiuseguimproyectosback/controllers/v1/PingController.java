28b2fd9b0eacd5c091318cf0fbb5db638abf74b9
DavidTorres-sys <david.torresg@udea.edu.co>
2024-12-17 11:13:48 -0500

    swagger ui and database conection

--- src/main/java/com/udea/siiuseguimproyectosback/controllers/v1/PingController.java
+++ src/main/java/com/udea/siiuseguimproyectosback/controllers/v1/PingController.java
@@ -1,5 +1,8 @@
 package com.udea.siiuseguimproyectosback.controllers.v1;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/v1/health")
-public class HealthController {
+public class PingController {
 
     /**
      * Health check endpoint to verify that the API is running.
      *
      * @return A simple confirmation message that the API is running.
      */
+    @Operation(summary = "Health Check",
+            description = "Returns a simple message to indicate that the API is operational.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "API is running successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @GetMapping("/")
-    public ResponseEntity<String> health() {
+    public ResponseEntity<String> check() {
         return new ResponseEntity<>("API is running", HttpStatus.OK);
     }
 }
